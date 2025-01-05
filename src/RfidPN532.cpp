a5952c51b40c63d1778c58723ad868bdf914f3ea
Simon Egli <4662005+Deadolus@users.noreply.github.com>
2025-01-05 13:23:04 +0100

    Power Off device when RFID not found
    Does recover RFID in most cases, sometimes happens that RFID was not
    found. Seems to relate to switching bluetooth modes.

--- src/RfidPN532.cpp
+++ src/RfidPN532.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "settings.h"
 #include "Rfid.h"
+#include "Power.h"
 #include "Log.h"
 #include "MemX.h"
 #include "Queues.h"
@@ -50,6 +51,7 @@
             if (!versiondata) {
                 Serial.print("Didn't Find PN53x Module HALT!!!");
 
+				Power_PeripheralOff();
         		System_RequestSleep();
                 while (1); // Halt
             }
